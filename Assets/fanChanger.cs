4b72213648ffc3d495480129756ba93dcbe2e054
Elhaj Elhaithem <haithemelhadj42@gmail.com>
2024-08-20 18:57:03 +0100

    coomit

--- Assets/fanChanger.cs
+++ Assets/fanChanger.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class fanChanger : MonoBehaviour
 {
-    
+    public AreaEffector2D aff1; 
+    public AreaEffector2D aff2;
+    public float direction;
+
+    private void Update()
+    {
+        aff1.forceAngle = transform.rotation.z;
+        aff2.forceAngle = transform.rotation.z;
+    }
 }
